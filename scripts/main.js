4d89ef9db14b3119611cb6c23b8a8c517cd54f07
StevenLzn <stevenlozano17@gmail.com>
2020-07-23 11:18:58 -0500

    Agregada funcionalidad de seleccionar mes. agregado algoritmo de organización de dias en el calendario, solucionados errores de diseño

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ var actual = new Date();
 var day = actual.getDate();
 var month = actual.getMonth() + 1;
 var year = actual.getFullYear();
+var page = 0;
 var MONTHS = [
     { id: "1", title: "ENERO", description: "Enero", days: 31 },
     { id: "2", title: "FEBRERO", description: "Febrero", days: 28 },
@@ -18,27 +19,33 @@ var MONTHS = [
 ]
 
 window.onload = function init() {
-    document.getElementById("month").textContent = MONTHS[month - 1].description;
     document.getElementById("year").textContent = year;
+    var body = document.createElement('tbody');
+    body.classList.add("list");
+    let container = document.getElementById("list-container");
+    container.appendChild(body);
     ListDays();
 }
 
-function ListDays(){
+function ListDays() {
     var day = 1;
     var counter = 0;
-    var container = document.getElementById("day-list");
+    var selectedYear = year;
+    var daysQuantity = MONTHS[month - 1].days;
+    var body = document.getElementsByClassName("list")[0];
+    document.getElementsByClassName("list")[0].innerHTML = '';
+    document.getElementById("month").textContent = MONTHS[month - 1].description;
 
     if (month >= 3) {
         month -= 2;
     } else {
         month += 10;
     }
-    if ((month === 11) || (month === 12)) {
-        year--;
+    if ((month == 11) || (month == 12)) {
+        selectedYear--;
     }
-
-    const k = year % 100;
-    const j = parseInt(year / 100);
+    const k = selectedYear % 100;
+    const j = parseInt(selectedYear / 100);
 
     let d = parseInt(2.6 * month - .2);
     d += parseInt(1 + k);
@@ -48,34 +55,66 @@ function ListDays(){
     d -= parseInt(2 * j);
     d %= 7;
 
-    if (year >= 1700 && year <= 1751) {
+    if (selectedYear >= 1700 && selectedYear <= 1751) {
         d -= 3;
     } else {
-        if (year <= 1699) {
+        if (selectedYear <= 1699) {
             d -= 4;
         }
     }
     if (d < 0) {
         d += 7;
     }
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 6; i++) {
         var row = document.createElement("tr")
         for (let j = 0; j < 7; j++) {
             if (i == 0 && j < d) {
                 var cell = document.createElement("td")
                 row.appendChild(cell)
             } else {
-                if (counter < MONTHS[month - 1].days) {
+                if (counter < daysQuantity) {
                     counter++;
                     var cell = document.createElement("td")
                     var cellText = document.createTextNode(day)
                     cell.classList.add("cell-day")
                     cell.appendChild(cellText)
                     row.appendChild(cell)
-                    day += 1;
+                    day++;
                 }
             }
         }
-        container.appendChild(row)
+        body.appendChild(row)
     }
+}
+
+function monthsClick() {
+    let counter = 0;
+    let body = document.getElementsByClassName("list")[0];
+    let container = document.getElementById("list-container");
+    document.getElementsByClassName("list")[0].innerHTML = '';
+    document.getElementsByClassName("days-title")[0].style.display = "none";
+    document.getElementById("month").style.display = "none";
+
+    container.appendChild(body);
+    for (let i = 0; i < 4; i++) {
+        let row = document.createElement("tr")
+        for (let j = 0; j < 3; j++) {
+            let cell = document.createElement("td")
+            let cellText = document.createTextNode(MONTHS[counter].description)
+            cell.addEventListener("click", monthClick)
+            cell.param = counter;
+            cell.classList.add("cell-month");
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+            counter++;
+        }
+        body.appendChild(row)
+    }
+}
+
+function monthClick(e) {
+    document.getElementsByClassName("days-title")[0].style.display = "table-row";
+    document.getElementById("month").style.display = "inline-block";
+    month = e.target.param + 1;
+    ListDays()
 }
